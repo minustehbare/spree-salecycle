bca9948381c7e254560c0f1a2b82c713cc1254d5
Minus <minustehbare@gmail.com>
2011-06-09 15:05:45 -0400

    remove old hooks and replace with Deface overrides

--- lib/spree_salecycle_hooks.rb
+++ lib/spree_salecycle_hooks.rb
@@ -1,7 +1,23 @@
 class SpreeSalecycleHooks < Spree::ThemeSupport::HookListener
   # custom hooks go here
-  insert_after :outside_cart_form, 'shared/salecycle'
-  insert_after :checkout_summary_box, 'shared/salecycle'
-  insert_after :order_details_total, 'shared/salecycle'
-  insert_after :admin_configurations_menu, 'salecycle_settings'
+  Deface::Override.new(:virtual_path => "orders/edit",
+                       :name => "salecycle_outside_cart_form",
+                       :insert_after => "[data-hook='outside_cart_form'], #outside_cart_form[data-hook]",
+                       :partial => "shared/salecycle",
+                       :disabled => false)
+  Deface::Override.new(:virtual_path => "checkout/edit",
+                       :name => "salecycle_checkout_summary_box",
+                       :insert_after => "[data-hook='checkout_summary_box'], #checkout_summary_box[data-hook]",
+                       :partial => "shared/salecycle",
+                       :disabled => false)
+  Deface::Override.new(:virtual_path => "shared/_order_details",
+                       :name => "salecycle_order_details_total",
+                       :insert_after => "[data-hook='order_details_total'], #order_details_total[data-hook]",
+                       :partial => "shared/salecycle",
+                       :disabled => false)
+  Deface::Override.new(:virtual_path => "admin/configurations/index",
+                       :name => "salecycle_admin_configurations_menu",
+                       :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
+                       :partial => "salecycle_settings",
+                       :disabled => false)
 end
