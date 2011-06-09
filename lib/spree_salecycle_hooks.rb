28870d71128460832598952c96ca45a3ca8d9ffa
Minus <minustehbare@gmail.com>
2011-06-09 08:14:13 -0400

    adding the other hooks

--- lib/spree_salecycle_hooks.rb
+++ lib/spree_salecycle_hooks.rb
@@ -1,5 +1,7 @@
 class SpreeSalecycleHooks < Spree::ThemeSupport::HookListener
   # custom hooks go here
-  insert_after :outside_cart_form, 'salecycle'
+  insert_after :outside_cart_form, 'shared/salecycle'
+  insert_after :checkout_summary_box, 'shared/salecycle'
+  insert_after :order_detailsm, 'shared/salecycle'
   insert_after :admin_configurations_menu, 'salecycle_settings'
 end
